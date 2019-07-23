d9994b1745a3f3df6a23641bba25b93057415177
Phil Reynolds <phil.reynolds@mobilise.cloud>
2019-07-23 13:25:27 +0100

    seed database with users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.create(
+        email: 'testadmin@purupleriver.dev',
+        first_name: 'Admin',
+        last_name: 'Reynolds',
+        password: 'admintest',
+        admin: 'true'
+)
+100.times do
+  User.create(
+       email: Faker::Internet.unique.free_email,
+       first_name: Faker::Name.first_name,
+       last_name: Faker::Name.last_name,
+       password: 'test123mob',
+       address_company: Faker::Company.name,
+       address_1: Faker::Address.street_address,
+       address_2: Faker::Address.community,
+       address_town: Faker::Address.city,
+       address_county: Faker::Address.state
+  )
+end
