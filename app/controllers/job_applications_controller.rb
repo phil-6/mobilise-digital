0ef8ff253a8f52a78714dbe5ccac091d2fe931a5
Phil Reynolds <phil.reynolds@mobilise.cloud>
2019-08-12 15:19:59 +0100

    update job application controller

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,12 +1,35 @@
-class JobsController < ApplicationController
+class JobApplicationsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_job, only: [:create]
+  before_action :set_job, only: :create
 
+  def index
+    @job_applications = JobApplication.where(:user_id => current_user.id)
+  end
+
+  def new
+    if JobApplication.exists?(user_id: current_user.id, job_id: params[:job_id])
+      flash[:error] = 'You have already applied for this job'
+      redirect_to @job
+    end
+    @job_application = JobApplication.new
+  end
+
+  # POST /job/:job_id/applications
+  def create
+    if current_user.job_applications.create!(rating_params)
+      redirect_to @job
+    else
+      render 'edit'
+    end
+  end
 
   private
 
   def set_job
-    @job = Job.find(params[:id])
+      #@job = Job.find(@job_applications.job_id) if @job_applications
+      @job = Job.find(params[:job_id])
   end
+
+
 
 end
