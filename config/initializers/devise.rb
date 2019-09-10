fc977d1b19524d6a233ba592e61b22016505d4af
Garreth Davies <garreth.davies@mobilise.cloud>
2019-09-10 10:23:57 +0100

    Email config changes

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'phil.reynolds@mobilise.cloud'
+  config.mailer_sender = 'donotreply@mobilise.cloud'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
