ee94bd679cfeac50614ee329ec8f238e2f10ac11
Phil Reynolds <phil.reynolds@mobilise.cloud>
2019-10-08 15:35:05 +0100

    updates root routes for when users are are not authenitcated

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:main]
 
   def main
-    if user_signed_in?
+    unless user_signed_in?
+      redirect_to new_user_session_path
+    else
       redirect_to dashboard_path
     end
   end
