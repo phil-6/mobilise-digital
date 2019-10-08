a6bcc9ab516206d2ead36013d23b2b8ac96500e0
Phil Reynolds <phil.reynolds@mobilise.cloud>
2019-10-08 11:10:41 +0100

    add email to mailer preview

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -1,8 +1,15 @@
 class DeviseMailerPreview < ActionMailer::Preview
+  ### Preview all emails at http://localhost:3000/rails/mailers
+
   # def welcome_email
   #   UserMailer.with(user: User.first).welcome_email
   # end
+
   def confirmation_instructions
     Devise::Mailer.confirmation_instructions(User.first, "faketoken")
+  end
+
+  def reset_password_instructions
+    MyMailer.reset_password_instructions(User.first, "faketoken", {})
   end
 end
