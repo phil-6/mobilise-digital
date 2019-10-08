bdd936c8f3be38a409dbbfd223d8c195a7bef47f
Phil Reynolds <phil.reynolds@mobilise.cloud>
2019-10-08 11:12:30 +0100

    remove unecassary welcome_email code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,9 +3,9 @@ class UserMailer < ApplicationMailer
   default from: 'Mobilise Digital <members@mobilise.digital>'
   layout 'mailer'
 
-  def welcome_email
-    @user = params[:user]
-    @url  = 'https://mobilise.digital/login'
-    mail(to: @user.email,from: 'Mobilise Digital <members@mobilise.digital>', bcc:'phil.reynolds@mobilise.cloud', subject: 'Welcome to Mobilise Digital')
-  end
+  # def welcome_email
+  #   @user = params[:user]
+  #   @url  = 'https://mobilise.digital/login'
+  #   mail(to: @user.email,from: 'Mobilise Digital <members@mobilise.digital>', bcc:'phil.reynolds@mobilise.cloud', subject: 'Welcome to Mobilise Digital')
+  # end
 end
