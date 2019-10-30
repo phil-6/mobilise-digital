4dec5b26c3eac4c17148605838452c32752d6989
Phil Reynolds <phil.reynolds@mobilise.cloud>
2019-10-30 13:35:25 +0000

    create referrals migration

---
+++ db/migrate/004_create_referrals.rb
@@ -0,0 +1,11 @@
+class CreateReferrals < ActiveRecord::Migration[5.2]
+  def change
+    create_table :referrals do |t|
+      t.belongs_to :user
+      t.text :email
+      t.string :status,       default: 'referred'
+      t.int :referred_user_id
+      t.timestamps
+    end
+  end
+end
