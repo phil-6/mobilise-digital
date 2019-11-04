1441f7a1bfeaa9f6af78dc7532309bd797113ce7
Phil Reynolds <phil.reynolds@mobilise.cloud>
2019-11-04 15:32:35 +0000

    change default referral status for capitalisation

--- db/migrate/004_create_referrals.rb
+++ db/migrate/004_create_referrals.rb
@@ -3,7 +3,7 @@ class CreateReferrals < ActiveRecord::Migration[5.2]
     create_table :referrals do |t|
       t.belongs_to :user
       t.text :email
-      t.string :status,       default: 'referred'
+      t.string :status,       default: 'Referred'
       t.integer :referred_user_id
       t.timestamps
     end
